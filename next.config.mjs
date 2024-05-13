31402e83b67afa9fef8b37a8dc9d8e1aeb0cb2b7
Souvik <souvikroy1999ab@gmail.com>
2024-05-13 09:51:32 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    // Set up an alias for the 'src' directory
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+
+    // Return the updated config
+    return config;
+  },
 };
 
 export default nextConfig;
